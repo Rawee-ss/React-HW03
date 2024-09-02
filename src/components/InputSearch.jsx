2f0f161218c3c63b21aef3a531bbee709e27cb9d
rawee-ss <raweeporn.ss27@gmail.com>
2024-09-02 17:34:49 +0700

    hw03

---
+++ src/components/InputSearch.jsx
@@ -0,0 +1,9 @@
+
+
+export default function InputSearch({ hdlChange }) {
+  return (
+    <div>
+        <input onChange={hdlChange} className="bg-gray-200 py-2 px-4 rounded-xl" type="text" />
+    </div>
+  )
+}
---
+++ src/components/ListData.jsx
@@ -0,0 +1,8 @@
+
+export default function ListData(props) {
+
+    const {item} = props
+  return (
+    <li>{item.title} | {item.category} | {item.price}</li>
+  )
+}
